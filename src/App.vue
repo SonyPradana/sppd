c8016808cb2616c5982a0b2659217445a977f842
SonyPradana <sonypradana@gmail.com>
2023-04-03 22:20:31 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Jentik from './components/Forms/Jentik.vue'
+</script>
+
+<template>
+  <h1 class="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-950 dark:text-gray-300 md:text-5xl lg:text-6xl ">SPPD Maker</h1>
+  <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Alat untuk membuat sppd otomatis.</p>
+
+  <Jentik></Jentik>
+</template>
+
---
+++ src/components/Forms/Jentik.vue
@@ -0,0 +1,43 @@
+<script setup>
+</script>
+
+<template>
+  
+<form>
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+      <div>
+        <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+        <input type="text" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Angger" required>
+      </div>
+      <div>
+        <label for="nip" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIP</label>
+        <input type="text" id="nip" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1994030500000000" required>
+      </div>
+      <div>
+        <label for="pangkat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pangkat Golongan</label>
+        <input type="text" id="pangkat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="IVd" required>
+      </div>
+      <div>
+        <label for="jenis" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis</label>
+        <input type="text" id="jenis" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Epidemolog" required>
+      </div>
+    </div>
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+      <div class="mb-6">
+        <label for="tanggal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal</label>
+        <input type="text" id="tanggal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="dd-mm-yyyyy" required>
+      </div> 
+      <div class="mb-6">
+        <label for="Desa" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Desa</label>
+        <input type="text" id="Desa" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Desa Branjang" required>
+      </div>
+    </div>
+        <div class="mb-6">
+          <label for="Tujuan" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tujuan</label>
+          <input type="text" id="Tujuan" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pemantauan Jentik Nyamuk" required>
+        </div>
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buat</button>
+</form>
+
+</template>
+
