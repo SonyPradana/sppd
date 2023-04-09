26e1bbce5a04a0633332cd39032dd66379cde2df
SonyPradana <sonypradana@gmail.com>
2023-04-10 00:14:19 +0700

    refactor: date format

--- src/sppd/jentik.js
+++ src/sppd/jentik.js
@@ -43,14 +43,12 @@ async function save(dates, {nama, nip, golongan, jabatan, tujuan, alamat}) {
  * Create sheat from workbook
  * 
  * @param {ExcelJS.Workbook} workbook 
- * @param {Date} date 
+ * @param {Date} tanggal 
  * @param {object} data 
  */
-async function create(workbook, date, {nama, nip, golongan, jabatan, tujuan, alamat}) {
-  // filen name
-  const day = date.getDate()
-  const month = date.getMonth() + 1
-  const year = date.getFullYear()
+async function create(workbook, tanggal, {nama, nip, golongan, jabatan, tujuan, alamat}) {
+  const short_date = dayjs(tanggal).format('D-M-YYYY')
+  const date = dayjs(tanggal).format('DD MMMM YYYY')
 
   const ws = workbook.getWorksheet('data')
   ws.getCell('B1').value = nama
@@ -67,8 +65,8 @@ async function create(workbook, date, {nama, nip, golongan, jabatan, tujuan, ala
   cp_surat_tugas.model = Object.assign(surat_tugas.model, {
     mergeCells: surat_tugas.model.merges
   })
-  cp_surat_tugas.name = `surat_tugas ${day}-${month}-${year}`
-  cp_surat_tugas.getCell('E23').value = dayjs(date).format('DD MMMM YYYY')
+  cp_surat_tugas.name = `surat_tugas ${short_date}`
+  cp_surat_tugas.getCell('E23').value = date
   cp_surat_tugas.getCell('E23').numFmt = '[$-id-ID]dd mmmm yyyy;@'
 
   // cp sppd_depan
@@ -78,8 +76,8 @@ async function create(workbook, date, {nama, nip, golongan, jabatan, tujuan, ala
   cp_sppd_depan.model = Object.assign(sppd_depan.model, {
     mergeCells: sppd_depan.model.merges
   })
-  cp_sppd_depan.name = `sppd_depan ${day}-${month}-${year}`
-  cp_sppd_depan.getCell('D22').value = dayjs(date).format('DD MMMM YYYY')
+  cp_sppd_depan.name = `sppd_depan ${short_date}`
+  cp_sppd_depan.getCell('D22').value = date
   cp_sppd_depan.getCell('D22').numFmt = '[$-id-ID]dd mmmm yyyy;@'
 
   // cp sppd_belakang
@@ -89,8 +87,8 @@ async function create(workbook, date, {nama, nip, golongan, jabatan, tujuan, ala
   cp_sppd_belakang.model = Object.assign(sppd_belakang.model, {
     mergeCells: sppd_belakang.model.merges
   });
-  cp_sppd_belakang.name = `sppd_belakang ${day}-${month}-${year}`;
-  cp_sppd_belakang.getCell('F7').value = dayjs(date).format('DD MMMM YYYY')
+  cp_sppd_belakang.name = `sppd_belakang ${short_date}`;
+  cp_sppd_belakang.getCell('F7').value = date
   cp_sppd_belakang.getCell('F7').numFmt = '[$-id-ID]dd mmmm yyyy;@'
 }
 
