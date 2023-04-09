ca205d41a4e78a5d30f0a3e2ec0c0db26be24074
Angger Pradana <sonypradana@gmail.com>
2023-04-09 07:58:23 +0700

    Down

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,6 @@ import Jentik from './components/Forms/Jentik.vue'
   <h1 class="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-950 dark:text-gray-300 md:text-5xl lg:text-6xl ">SPPD Maker</h1>
   <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Alat untuk membuat sppd otomatis.</p>
 
-  <Jentik></Jentik>
+  <h1>500 - Dalam Pengembangan</h1>
 </template>
 
