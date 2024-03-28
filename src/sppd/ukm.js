d915d863010fdaaec2401eca05892bf29a6bfaac
SonyPradana <sonypradana@gmail.com>
2024-03-28 13:58:49 +0700

    feat: auto creating nomer surat tugas

--- src/sppd/ukm.js
+++ src/sppd/ukm.js
@@ -24,6 +24,7 @@ async function save(dates, { nama, nip, golongan, jabatan, tujuan, alamat }) {
     const workbook = new ExcelJS.Workbook()
     await workbook.xlsx.load(await getResponseAsBuffer(excel))
 
+    let index = 1
     for (const date of dates._rawValue) {
         await createWorksheet(workbook, date, {
             nama,
@@ -32,6 +33,7 @@ async function save(dates, { nama, nip, golongan, jabatan, tujuan, alamat }) {
             jabatan,
             tujuan,
             alamat,
+            nomor: index++
         })
     }
 
@@ -63,15 +65,15 @@ async function save(dates, { nama, nip, golongan, jabatan, tujuan, alamat }) {
 async function createWorksheet(
     workbook,
     tanggal,
-    { nama, nip, golongan, jabatan, tujuan, alamat }
+    { nama, nip, golongan, jabatan, tujuan, alamat, nomor }
 ) {
     const short_date = dayjs(tanggal).format('D-M-YYYY')
     const date       = dayjs(tanggal).format('DD MMMM YYYY')
 
-    createDataWorksheet(workbook, { nama, nip, golongan, jabatan, tujuan, alamat })
-    createSuratTugasWorksheet(workbook, short_date, date, nama[2])
-    createSppdDepanWorksheet(workbook, short_date, date)
-    createSppdBelakangWorksheet(workbook, short_date, date)
+    createDataWorksheet(workbook, { nama, nip, golongan, jabatan, tujuan, alamat, nomor })
+    createSuratTugasWorksheet(workbook, short_date, date, nomor, nama[2])
+    createSppdDepanWorksheet(workbook, short_date, date, nomor)
+    createSppdBelakangWorksheet(workbook, short_date, date, nomor)
 }
 
 /**
@@ -79,8 +81,9 @@ async function createWorksheet(
  *
  * @param {ExcelJS.Workbook} workbook
  * @param {object} data
+ * @param {int} nomor
  */
-function createDataWorksheet(workbook, { nama, nip, golongan, jabatan, tujuan, alamat }) {
+function createDataWorksheet(workbook, { nama, nip, golongan, jabatan, tujuan, alamat, nomor }) {
     const ws = workbook.getWorksheet('data')
     ws.getCell('B1').value = nama[1] ?? ''
     ws.getCell('B2').value = nip[1] ?? ''
@@ -88,6 +91,7 @@ function createDataWorksheet(workbook, { nama, nip, golongan, jabatan, tujuan, a
     ws.getCell('B4').value = jabatan[1] ?? ''
     ws.getCell('B6').value = tujuan ?? ''
     ws.getCell('B7').value = alamat ?? ''
+    ws.getCell(`B${7 + nomor}`).value = nomor
     ws.getCell('C1').value = nama[2] ?? ''
     ws.getCell('C2').value = nip[2] ?? ''
     ws.getCell('C3').value = golongan[2] ?? ''
@@ -100,9 +104,10 @@ function createDataWorksheet(workbook, { nama, nip, golongan, jabatan, tujuan, a
  * @param {ExcelJS.Workbook} workbook
  * @param {string} short_date
  * @param {string} date
+ * @param {int} nomor
  * @param {string} secondName
  */
-function createSuratTugasWorksheet(workbook, short_date, date, secondName) {
+function createSuratTugasWorksheet(workbook, short_date, date, nomor, secondName) {
     const surat_tugas = workbook.getWorksheet('surat_tugas')
     let cp_surat_tugas = workbook.addWorksheet('cp')
 
@@ -110,6 +115,7 @@ function createSuratTugasWorksheet(workbook, short_date, date, secondName) {
         mergeCells: surat_tugas.model.merges,
     })
     cp_surat_tugas.name = `surat_tugas ${short_date}`
+    cp_surat_tugas.getCell('B7').value = { formula: `"Nomor : 449.1/"& (data!B${7 + nomor}) &"/UKM/2024"`, result: 'Nomor : 449.1/ /UKM/2024' }
     cp_surat_tugas.getCell('E23').value = date
     cp_surat_tugas.getCell('E23').numFmt = '[$-id-ID]dd mmmm yyyy;@'
     // remove unsed cell
@@ -128,8 +134,9 @@ function createSuratTugasWorksheet(workbook, short_date, date, secondName) {
  * @param {ExcelJS.Workbook} workbook
  * @param {string} short_date
  * @param {string} date
+ * @param {int} nomor
  */
-function createSppdDepanWorksheet(workbook, short_date, date) {
+function createSppdDepanWorksheet(workbook, short_date, date, nomor) {
     const sppd_depan = workbook.getWorksheet('sppd_depan')
     let cp_sppd_depan = workbook.addWorksheet('cp')
 
@@ -137,6 +144,7 @@ function createSppdDepanWorksheet(workbook, short_date, date) {
         mergeCells: sppd_depan.model.merges,
     })
     cp_sppd_depan.name = `sppd_depan ${short_date}`
+    cp_sppd_depan.getCell('A8').value = { formula: `"Nomor : 449.1/"& (data!B${7 + nomor}) &"/UKM/2024"`, result: 'Nomor : 449.1/ /UKM/2024' }
     cp_sppd_depan.getCell('D22').value = date
     cp_sppd_depan.getCell('D22').numFmt = '[$-id-ID]dd mmmm yyyy;@'
     // fix boder not render as well
@@ -149,8 +157,9 @@ function createSppdDepanWorksheet(workbook, short_date, date) {
  * @param {ExcelJS.Workbook} workbook
  * @param {string} short_date
  * @param {string} date
+ * @param {int} nomor
  */
-function createSppdBelakangWorksheet(workbook, short_date, date) {
+function createSppdBelakangWorksheet(workbook, short_date, date, nomor) {
     const sppd_belakang = workbook.getWorksheet('sppd_belakang')
     let cp_sppd_belakang = workbook.addWorksheet('cp')
 
@@ -158,6 +167,7 @@ function createSppdBelakangWorksheet(workbook, short_date, date) {
         mergeCells: sppd_belakang.model.merges,
     })
     cp_sppd_belakang.name = `sppd_belakang ${short_date}`
+    cp_sppd_belakang.getCell('F5').value  = { formula: `": 449.1/"& (data!B${7 + nomor}) &"/UKM/2024"`, result: ': 449.1/ /UKM/2024' }
     cp_sppd_belakang.getCell('F7').value  = ': ' + date
     cp_sppd_belakang.getCell('D13').value = date
     cp_sppd_belakang.getCell('F7').numFmt = ': [$-id-ID]dd mmmm yyyy;@'
